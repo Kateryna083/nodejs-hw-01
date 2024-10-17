dabe577111609f29c398edb0d0c97ad7c3f0af9f
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2024-10-17 12:54:23 +0300

    done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+  const newContacts = Array(number).fill().map(createFakeContact);
+  const result = [...contacts, ...newContacts];
+  writeContacts(result);
+};
 
 generateContacts(5);
